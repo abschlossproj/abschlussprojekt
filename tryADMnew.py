c636c29fea020434f828b3d89b84064492c59dc8
matthiaslang <matthiaslang96+github@gmail.com>
2020-07-14 20:25:03 +0200

    alter Bug behoben, adMatrix anders falsch?

--- tryADMnew.py
+++ tryADMnew.py
@@ -1,11 +1,21 @@
 # -*- coding: utf-8 -*-
+'''
+In der adMatrix.npy hat (array([4983, 4998]),) weniger als k Einträge (mehr als k Einträge kein Problem)
+Gleiches Problem bei kleineren k und N, Fehler taucht öfters auf
+Neue Funktion indexfilter_3 hat das Problem scheinbar behoben
 
+if (np.sum(adMatrix, axis=0) < k).any():
+    print("Achtung: in adMatrix gibt Personen mit weniger als k Kontakten")
+spuckt keinen Fehler aus, aber 
+adMatrix.sum()-(5000*20) > 0
+also müssten es wieder zu wenige Kontakte sein???
+'''
 
 import numpy as np
 
 
-N = 1000 #5000 for testing less  # populationsize 
-k = 6 #20 for testing less    # figure of social interactions per day per person
+N = 5000   # populationsize 
+k = 20     # figure of social interactions per day per person
 # unterschiedliche Szenarien sollen gerechnet werden. Vorschlag: k=5,10,20 -> eine große Schleife?
 
 adMatrix = np.zeros((N, N), dtype=bool)    # matrix which shows social contacts
@@ -22,34 +32,8 @@ def indexfilter_3(row, matrix=adMatrix):
 
     
 for i in np.arange(N):    # loop for every row
-    potCon = indexfilter_2(i)    # list with potentional new contacs per row
+    potCon = indexfilter_3(i)    # list with potentional new contacs per row
     addCon = k - adMatrix[i,].sum()      # additional contacs for k entries in a row  
-    
-# Durch eine ungünstige Abfolge des Matrix Aufbaus, kann die Situation entstehen, dass nicht in jeder Zeile exakt k Kontakte möglich sind.
-    if addCon > 0 and len(potCon) > 0:
-        newCon = min(addCon, len(potCon))  # new social contacts     ## HIER WAR FEHLER IN ANGABE K MUSS DURCH addCon ERSETZT WERDEN
-        indizes = np.random.choice(potCon, newCon, replace=False)   # chooses random indices out of potential indices
-        adMatrix[i, indizes] = True   # obere Dreiecksmatrix der Kontakte
-        #adMatrix[indizes, i] = True   # um die Matrix symmetrisch zu machen
-
-if (np.sum(adMatrix, axis=0) < k).any():
-    print("Achtung: in adMatrix gibt Personen mit weniger als k Kontakten")
-        
-
-'''
-In der adMatrix.npy hat (array([4983, 4998]),) weniger als k Einträge (mehr als k Einträge kein Problem)
-Gleiches Problem bei kleineren k und N, Fehler taucht öfters auf
-Statt Debugging kleiner Workaround:
-'''
-
-''' Workaround klappt noch nicht
-
-if (np.sum(adMatrix, axis=0) != k).any():
-    print("Achtung: in adMatrix gibt es Zeilensummen ungleich k. Workaround...")
-########## Code von oben, klappt auch nicht########################
-    potCon = indexfilter_2(adMatrix, i)    # list with potentional new contacs per row
-    addCon = k - adMatrix[i,].sum()      # additional contacs for k entries in a row  
-
     
 # Durch eine ungünstige Abfolge des Matrix Aufbaus, kann die Situation entstehen, dass nicht in jeder Zeile exakt k Kontakte möglich sind.
     if addCon > 0 and len(potCon) > 0:
@@ -57,23 +41,17 @@ if (np.sum(adMatrix, axis=0) != k).any():
         indizes = np.random.choice(potCon, newCon, replace=False)   # chooses random indices out of potential indices
         adMatrix[i, indizes] = True   # obere Dreiecksmatrix der Kontakte
         adMatrix[indizes, i] = True   # um die Matrix symmetrisch zu machen
-#################################################################     
+if (np.sum(adMatrix, axis=0) < k).any():
+    print("Achtung: in adMatrix gibt Personen mit weniger als k Kontakten")
+        
 
-    if (np.sum(adMatrix, axis=0) != k).any():
-        print("Achtung: in adMatrix gibt es Zeilensummen ungleich k")
-    else:
-        print("Workaround erfolgreich, adMatrix vollständig")
-else:
-    print("In adMatrix sind alle Zeilensummen gleich k")
+np.save('adMatrix_Mat.npy', adMatrix)   # saves adMatrix
+#np.load('adMatrix.npy')             # loads adMatrix    
 
 
 '''
-
-
-#print(adMatrix)                      #for testing, also change N
-#np.save('adMatrix_Mat.npy', adMatrix)   # saves adMatrix
-#np.load('adMatrix.npy')             # loads adMatrix    
-
+Programm dauert bei mir nur Sekunden
+'''
 
 
 
