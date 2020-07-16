0cc34a5ccb7e221f232d9e7f418c0db61d47dc68
Matthias <66843529+matthias-10@users.noreply.github.com>
2020-07-16 15:32:34 +0200

    Create visualisation.py

---
+++ visualisation.py
@@ -0,0 +1,11 @@
+'''
+Es gibt "plotnine" für python, das hat fast die gleiche Grammatik wie ggplot2 für R.
+
+pip install plotnine
+
+https://plotnine.readthedocs.io/en/stable/index.html
+'''
+
+#to-do: documentation Matrix aus simul_2.py abspeichern und hier laden
+
+import plotnine
