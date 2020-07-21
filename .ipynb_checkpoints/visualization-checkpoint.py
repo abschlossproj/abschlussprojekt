d6efd1ee5bbfb2d49076f6f4a8d7f7ff131d2e5c
matthiaslang <matthiaslang96+github@gmail.com>
2020-07-21 08:11:41 +0200

    corrections for vis

--- .ipynb_checkpoints/visualization-checkpoint.py
+++ .ipynb_checkpoints/visualization-checkpoint.py
@@ -14,7 +14,7 @@ To-do: maybe shorten the datatables, when theres a boring "tail"
 
 #to-do: documentation Matrix aus simul_2.py abspeichern und hier laden
 
-from plotnine import ggplot, aes, geom_line, geom_col, facet_grid
+from plotnine import ggplot, aes, geom_line, geom_col, facet_grid,  scale_y_log10, geom_point, facet_wrap, geom_step
 import pandas as pd
 import numpy as np
 import math
@@ -34,8 +34,8 @@ for k in k_l:
 files = os.listdir('documentation_tables')
 docs = pd.DataFrame()
 #i=1
-for i, y in enumerate(files):
-    df = pd.read_csv(('documentation_tables' + '/' + files[i]), index_col=0) # Compatibility issues?
+for x, y in enumerate(files):
+    df = pd.read_csv(('documentation_tables' + '/' + files[x]), index_col=0) # Compatibility issues?
     #slice the parameters of the scenario from the filename
     df['k'] = int(y[21:23])
     df['m'] = int(y[25:27])
@@ -51,16 +51,40 @@ for i, y in enumerate(files):
         
         
 # facet über p, k, m,mr fix
- '''todo'''
-
-# line-graphs, die die totalen Infizierten über verschiedenen p,k vergleichen
 docm05 = docs[docs.m==5]
 g = ggplot(docm05, aes(x='days'))
 g + geom_line(aes(y='T')) + facet_grid(('k','p')) #in R not a tuple but ~
+# interesting: for (20,50) there's less deads in total than vor (10,25)
 
 
-#docs[(docs.k==20)&(docs.m==10)&(docs.p==25)]
+# line-graphs, die die totalen Infizierten über verschiedenen p,k vergleichen
+g + geom_point(aes(y=docm05.D, fill='p', size='k'))
+g + geom_line(aes(y='D', color='k', fill='k')) + facet_wrap(('p','k'))
+#for it to work in a single graph, one probably needs k and p as factors not doubles
+docm05_cp = docm05.copy()
+docm05_cp['k'] = docm05.k.astype(str)
+docm05_cp['p'] = docm05.p.astype(str)
+doc_interest = docm05_cp[docm05_cp['days']<10]
+g_int = ggplot(doc_interest, aes(x='days',y='D')) 
+g_int + geom_line(aes(colour='k')) + scale_y_log10() + facet_wrap('p')
 
+g_int + geom_line(aes(colour='k',linetype="p")) + scale_y_log10()
+#g_int + geom_point(aes(colour='k',shape="p")) 
+
+'''
+# ggplot(docm05, aes('days','D', colour='p')) + geom_line() + facet_wrap(('k')) #maybe works in R?
+### workaround
+docm05wide = docm05[['days', 'D', 'p','k']]
+# I am too dumb to pivot:
+df = docm05.iloc[:,[0,2,5,7]]
+docm05k05 = df[df.k == 5][['days','D','p']]
+docm05k10 = df[df.k == 10][['days','D','p']]
+docm05k20 = df[df.k == 20][['days','D','p']]
+
+.pivot( columns='D',values=['k','p'])
+docm05wide = docm05.pivot(columns='p')
+
+'''
 ############################
 '''
 
@@ -106,10 +130,12 @@ ggplot(doc_c, aes(x= "days")) \
             
 # logaithmierte D und T
 def log10(x):
-    if x>0:
+    if x>10:
         return x
+    elif x>0:
+        return 11
     else:
-        return 1
+        return 10 # 1 would be better, looks not good tho, and the base would look like zero
 doc_logT = doc['T'].apply(lambda x: math.log(log10(x),10)) #sehr langsam :/
 doc_logD = doc['D'].apply(lambda x: math.log(log10(x),10)) # -> np.log(x) (geht auch mit df/ser?!)
 doc_logDT = pd.DataFrame((doc['days'], doc_logT, doc_logD)).T
@@ -124,21 +150,28 @@ for i in np.arange(len(doc)-1)+1:
     D_nw.append(doc.loc[i-1,"H"]-doc.loc[i,"H"])
     R_nw.append(doc.loc[i,"R"]-doc.loc[i-1,"R"])
     T_nw.append(doc.loc[i,"T"]-doc.loc[i-1,"T"])
-doc_change = pd.DataFrame({'D': doc.loc[1:25,"D"],
+    
+doc_change = pd.DataFrame({'H': doc.loc[1:25,'H'],
+                           'D': doc.loc[1:25,"D"],
                            'G': np.add(doc.loc[1:25,"H"],doc.loc[1:25,"R"]), #Genesene+Healthy
                            'D_change': D_chng, 
-                           'D_new' :D_nw, 
+                           'D_new' :D_nw,
+                           'D_new_c': np.add(np.add(R_nw,T_nw),D_nw),
+                           'D_new_neg': np.negative(D_nw),
                            'R_new':R_nw,
+                           'R_new_c':np.add(R_nw,T_nw),
+                           'R_new_neg': np.negative(R_nw),
                            'days':doc.loc[1:25,'days'], 
                            'T':doc.loc[1:25,"T"],
                            'T_new':np.multiply(-1, T_nw),
-                           'sgR_new': (R_nw > T_nw),
-                           'sgD_change': np.sign(D_chng), #zeigt an, ob die Verringerung Kranker eher wegen Todes oder Genesungen kommt
+                           'T_new_pos':T_nw,
+                           'T_new_log': (np.log10(list(map(log10, T_nw)))),
                            'cumT':np.add(R_nw,T_nw)}) 
+
 ggplot(doc_change, aes(x='days'))  \
     + geom_col(aes(y="cumT"),fill='red',width=.6) \
     + geom_col(aes(y="D_new",alpha=.5)) \
-    + geom_col(aes(y="R_new",width=.6,fill='sgD_change')) \
+    + geom_col(aes(y="R_new",width=.6)) \
     + geom_line(aes(y='G'))
     #+ geom_line(aes(y="D_change"))
     #+ geom_col(aes(y="D_change", col='sgR_new')) #<-könnte später interessant werden, wenn in Simulationen die Ansteckungsraten langsamer sind, also sich D ändert, weil Leute sterben (-), genesen (-) und sich anstecken (+) 
@@ -160,3 +193,28 @@ Notizen vom 20.07:
 Grafiken:
 rote Column negativ: Tote/Tag, grüne positiv: Genesungen pro Tag, gelbe Transparent dahinter: Neuinfektionen, Linie: entweder D oder cumD
 '''
+
+'''
+alles als bar:
+cumR
+healthy
+}sodass die ein overlay bilden
+-D_new
+-T_new
+-R_new
+'''
+
+
+    #+ geom_line(aes(y="G",fill='3')) \
+    #+ geom_line(aes(y="H")) \
+ggplot(doc_change, aes(x='days'))  \
+    + geom_col(aes(y="D_new_c", fill= 1), width=0.4) \
+    + geom_col(aes(y='R_new_c', fill=2), width=0.4) \
+    + scale_y_log10() \
+    + geom_col(aes(y='T_new_log'),fill='red', width=0.4)
+
+ggplot(doc_change, aes(x='days'))  \
+    + geom_col(aes(y="D_new_c", fill= 1), width=0.4) \
+    + geom_col(aes(y='R_new_c', fill=2), width=0.4) \
+    + geom_point(aes(y='G'),fill='w') \
+    + geom_col(aes(y='T_new_pos'),fill='red', width=0.4)
